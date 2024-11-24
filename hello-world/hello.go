e46e8ab4d0c899a20bb8fc7f299724d425cd56c5
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-11-24 15:51:00 -0600

    added french language and tests
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,21 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name, language string) string {
 	if name == "" {
 		name = "world"
 	}
 
-	if language == spanish {
+	switch language {
+	case "Spanish":
 		return spanishHelloPrefix + name
+	case "French":
+		return frenchHelloPrefix + name
+	default:
+		return englishHelloPrefix + name
 	}
-
-	return englishHelloPrefix + name
 }
 
 func main() {
