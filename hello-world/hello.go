0b9ca94ada08858a9344354f6c6b1d9e60493f07
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-11-24 15:41:45 -0600

    added spanish language and tests
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -4,13 +4,18 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "world"
 	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,13 +4,18 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Trent")
+		got := Hello("Trent", "English")
 		want := "Hello, Trent"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
