281f91b518154c0474c4444f2e7e8cc1ed1a6895
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-01-11 23:45:07 -0600

    refactor of SumAll to use append
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- arrayslices/sum.go
+++ arrayslices/sum.go
@@ -10,11 +10,9 @@ func Sum(nums []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 
 	return sums
