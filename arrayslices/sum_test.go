115d59e9851bacf4d21382e7fed7eb49e6006ad6
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-01-13 23:41:41 -0600

    refactored and added helper to remove duplicate code in tests
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- arrayslices/sum_test.go
+++ arrayslices/sum_test.go
@@ -28,21 +28,23 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
 	t.Run("sum the tails of two slices", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
 
-		if !slices.Equal(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 
 	t.Run("try with empty slice", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
 
-		if !slices.Equal(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 }
