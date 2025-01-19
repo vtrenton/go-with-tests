db9ee3b7b7df1e4438c0eaf92dce10e0d8f84901
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-01-18 19:28:23 -0600

    started pointers and errors
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

---
+++ pointerserr/wallet_test.go
@@ -0,0 +1,16 @@
+package wallet
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balence()
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
