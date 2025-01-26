f7450ddb13cbcf3b3244a58e9a91fba76e91db7f
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-01-25 23:36:40 -0600

    added logic to not allow overdraws
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- pointerserr/wallet.go
+++ pointerserr/wallet.go
@@ -1,6 +1,9 @@
 package wallet
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -20,8 +23,13 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+
 	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
