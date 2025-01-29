64f093d79d8effde0e6c4f63e67f4fe99213280f
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-01-28 18:24:52 -0600

    implemented map search function
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this just is a test"}
+	dictionary := map[string]string{"test": "this is just a test"}
 
 	got := Search(dictionary, "test")
 	want := "this is just a test"
