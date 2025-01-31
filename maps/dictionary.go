c1918cb758d0316b366fb749ccdd64f5d52c24a1
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-01-30 18:36:23 -0600

    refactored functions
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,13 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
+	dictionary, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you are looking")
+	}
 	return d[word], nil
 }
