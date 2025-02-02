fe805a0ee28f8040eafc7f92415ec786edd41dcf
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-02-01 19:15:26 -0600

    refactored dictionary errors
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -12,14 +12,13 @@ func TestSearch(t *testing.T) {
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
+		_, got := dictionary.Search("unknown")
 
-		if err == nil {
+		if got == nil {
 			t.Fatal("expected to get an error")
 		}
 
-		assertStrings(t, err.Error(), want)
+		assertError(t, got, ErrNotFound)
 	})
 }
 
@@ -28,5 +27,12 @@ func assertStrings(t testing.TB, got, want string) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
