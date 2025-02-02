88fd85420a662c9692331ad27925cf03c8dfed23
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-02-01 18:57:46 -0600

    fixed search function
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -5,9 +5,9 @@ import "errors"
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
-	dictionary, ok := d[word]
+	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you are looking")
+		return "", errors.New("could not find the word you are looking for")
 	}
-	return d[word], nil
+	return definition, nil
 }
