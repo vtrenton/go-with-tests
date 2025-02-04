c50fb1a2038d5310ed069c6201283c6526d1880b
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-02-04 17:14:14 -0600

    added deptinjection section
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

---
+++ deptinjection/greeter_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	greet("Chris", &buffer)
+
+	got := buffer.String()
+	want := "Hello Chris"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+}
