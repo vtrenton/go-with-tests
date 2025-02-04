5b3edf19120d35c6b8818ce33139fde55bcff42e
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-02-03 21:37:14 -0600

    refactor update and add new error condition
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,8 +4,9 @@ type Dictionary map[string]string
 type DictionaryErr string
 
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you are looking for")
-	ErrWordExists = DictionaryErr("could not add because word already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you are looking for")
+	ErrWordExists       = DictionaryErr("could not add because word already exists")
+	ErrWordDoesNotExist = DictionaryErr("Could not perform operation on word because it does not exist")
 )
 
 func (e DictionaryErr) Error() string {
@@ -35,6 +36,16 @@ func (d Dictionary) Add(word, definition string) error {
 }
 
 func (d Dictionary) Update(word, definition string) error {
-	d[word] = definition
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
 	return nil
 }
