d953b1a23b712aa9b425b55ef0c4690189979803
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-02-04 17:23:08 -0600

    fixed some bugs and added Greet
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- deptinjection/di_test.go
+++ deptinjection/di_test.go
@@ -7,11 +7,12 @@ import (
 
 func TestGreet(t *testing.T) {
 	buffer := bytes.Buffer{}
-	greet("Chris", &buffer)
+	Greet(&buffer, "Chris")
 
 	got := buffer.String()
-	want := "Hello Chris"
+	want := "Hello, Chris"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
 }
