50be93a381a34bdbf330b38c6d48083e03d783b7
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-02-07 20:49:54 -0600

    added new functs for depency injections
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -9,6 +9,10 @@ import (
 
 const FinalWord = "Go!"
 const CountdownStart = 3
+const FinalWord = "Go!"
+const CountdownStart = 3
+const write = "write"
+const sleep = "sleep"
 
 type Sleeper interface {
 	Sleep()
@@ -26,6 +30,19 @@ type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
+}
+
+type SpyCountDownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountDownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountDownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
