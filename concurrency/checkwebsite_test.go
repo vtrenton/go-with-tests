b6860cff3d59f09ac6a52686f3d85a1a6a5f61c4
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-02-11 22:16:47 -0600

    started concurrency chapt - boilerplate build out
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

---
+++ concurrency/checkwebsite_test.go
@@ -0,0 +1,30 @@
+package concurrency
+
+import (
+	"slices"
+	"testing"
+)
+
+func mockWebsiteChecker(url string) bool {
+	return url != "waat://furhurterwe.geds"
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites []string {
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	want := map[string]bool {
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !slices.Equal(want, got) {
+		t.Fatalf("wanted %q got %q", want, got)
+	}
+}
