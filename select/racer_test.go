98e7056896fcb0ef3d4ece50083c60bd9261acfe
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-03-04 23:45:37 -0600

    refactored testing adding mock http server
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,10 +1,23 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestWebSiteRacer(t *testing.T) {
-	slowURL := "https://facebook.com"
-	fastURL := "https://quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -12,4 +25,7 @@ func TestWebSiteRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
