d2ab003e5a6f0bd03e2336adb1199099e75688ed
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-03-08 14:50:16 -0600

    began writing test for refelctive function
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

---
+++ reflection/walk.go
@@ -0,0 +1,6 @@
+package main
+
+func walk(x interface{}, fn func(input string)) []string {
+	var temp []string
+	return temp
+}
---
+++ reflection/walk_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Trent"},
+			[]string{"Trent"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !slices.Equal(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
