59773f5c84d76467c7686edb4a6f3a3039ba8f22
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-03-09 23:38:29 -0500

    refactored to pass tests
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- reflection/walk.go
+++ reflection/walk.go
@@ -8,8 +8,13 @@ func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
+
 		if field.Kind() == reflect.String {
 			fn(field.String())
+		}
+
+		if field.Kind() == reflect.Struct {
+			walk(field.Interface(), fn)
 		}
 	}
 }
