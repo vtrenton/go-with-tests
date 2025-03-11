0893350fb32e17130f3cf36e9101c31d96f819e3
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-03-11 00:57:22 -0500

    refactored splitting code into function
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- reflection/walk.go
+++ reflection/walk.go
@@ -5,11 +5,7 @@ import (
 )
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
-
-	if val.Kind() == reflect.Pointer {
-		val = val.Elem()
-	}
+	val := getValue(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
@@ -21,4 +17,14 @@ func walk(x interface{}, fn func(input string)) {
 			walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
 }
