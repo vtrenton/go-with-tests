7f00dc8d4eddc8d38c7a97cbdf225f673fd958e4
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-03-17 22:49:41 -0500

    fixed bug in assertCounter checking for 3 not want
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- sync/counter_test.go
+++ sync/counter_test.go
@@ -36,7 +36,7 @@ func TestCounter(t *testing.T) {
 
 func assertCounter(t testing.TB, got Counter, want int) {
 	t.Helper()
-	if got.Value() != 3 {
+	if got.Value() != want {
 		t.Errorf("got %d want %d", got.Value(), want)
 	}
 }
