01ac3b6b6992de2fe9b96a4fe46db91a509c7724
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-03-24 22:56:24 -0500

    refactored negative test SpyResponseWriter
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- context/server_test.go
+++ context/server_test.go
@@ -2,6 +2,7 @@ package context
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -12,6 +13,24 @@ import (
 type SpyStore struct {
 	response string
 	t        *testing.T
+}
+
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
 }
 
 func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
@@ -76,22 +95,23 @@ func TestServer(t *testing.T) {
 		//store.AssertWasNotCancelled()
 	})
 
-	//	t.Run("tells store to cancel work if cancel is requested", func(t *testing.T) {
-	//		data := "hello, world"
-	//		store := &SpyStore{response: data}
-	//		svr := Server(store)
-	//
-	//		request := httptest.NewRequest(http.MethodGet, "/", nil)
-	//
-	//		cancellingCtx, cancel := context.WithCancel(request.Context())
-	//		time.AfterFunc(5*time.Millisecond, cancel)
-	//		request = request.WithContext(cancellingCtx)
-	//
-	//		response := httptest.NewRecorder()
-	//
-	//		svr.ServeHTTP(response, request)
-	//
-	//		store.AssertWasCancelled()
-	//
-	// })
+	t.Run("tells store to cancel work if cancel is requested", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := &SpyResponseWriter{}
+
+		svr.ServeHTTP(response, request)
+
+		if response.written {
+			t.Error("a response should not have been written")
+		}
+	})
 }
