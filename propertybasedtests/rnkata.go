c80dc2b8fad64180c28ff6a61d54e0dafe4730d2
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-04-03 23:34:08 -0500

    added the tests up to 1000 to finish outgit add .
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- propertybasedtests/rnkata.go
+++ propertybasedtests/rnkata.go
@@ -10,8 +10,13 @@ type RomanNumeral struct {
 }
 
 var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
 	{50, "L"},
-	{49, "IL"},
 	{40, "XL"},
 	{10, "X"},
 	{9, "IX"},
